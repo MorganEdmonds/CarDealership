d1aba3bf5a0a914aa853e3d4dccd5e704bd41515
MorganEdmonds <morganedmonds@gmail.com>
2025-10-24 15:51:42 -0400

    adding getters and setters and a transaction csv file

--- src/main/java/com/pluralsight/Dealership.java
+++ src/main/java/com/pluralsight/Dealership.java
@@ -2,9 +2,44 @@ package com.pluralsight;
 
 public class Dealership {
 
+// This is what a dealership is made up of.
+
     public String name;
    public String address;
    public String phone;
+
+//CONSTRUCTOR.
+    public Dealership(String name, String address, String phone ){
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+    }
+
+//Create getters and setters.
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 
 
 }
--- src/main/java/com/pluralsight/Vehicle.java
+++ src/main/java/com/pluralsight/Vehicle.java
@@ -2,4 +2,89 @@ package com.pluralsight;
 
 public class Vehicle {
 
+// This is how a Vehicle is made.
+
+    private int vin;
+    private int year;
+    private String name;
+    private String vehicleType;
+    private String color;
+    private int odometer;
+    private double price;
+
+
+    // Create methods for each
+
+    public Vehicle (String name, String vehicleType, String color, int vin, int year, int odometer, double price){
+
+        // CONSTRUCTOR.
+        this.vin = vin;
+        this.year = year;
+        this.name = name;
+        this.vehicleType = vehicleType;
+        this.color = color;
+        this.odometer = odometer;
+        this.price = price;
+
+    }
+
+    //Create getters and setters.
+
+    public int getVin() {
+        return vin;
+    }
+
+    public void setVin(int vin) {
+        this.vin = vin;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getVehicleType() {
+        return vehicleType;
+    }
+
+    public void setVehicleType(String vehicleType) {
+        this.vehicleType = vehicleType;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getOdometer() {
+        return odometer;
+    }
+
+    public void setOdometer(int odometer) {
+        this.odometer = odometer;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+
 }
